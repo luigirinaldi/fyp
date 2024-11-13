6a6e798394ce9a75f29235a9339f3cb36ea937bb
luigirinaldi <luigirinaldi@users.noreply.github.com>
2024-11-13 22:27:40 +0000

    broken state

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use egg::*;
 
 type Num = num::BigRational;
@@ -18,8 +20,8 @@ define_language! {
         // Numbers
         Num(Num),
         // variables on which the operators operate
-        Var(Symbol),
         Bool(bool),
+        Var(Symbol),
     }
 }
 
@@ -48,11 +50,22 @@ fn rules() -> Vec<Rewrite<ModIR, ModAnalysis>> {
         // normal arithmetic
         rewrite!("add-comm";    "(+ ?a ?b)" => "(+ ?b ?a)"),
         rewrite!("add-assoc";   "(+ (+ ?a ?b) ?c)" => "(+ ?a (+ ?b ?c))"),
+        rewrite!("mul-comm";    "(* ?a ?b)" => "(* ?b ?a)"),
+        rewrite!("mul-assoc";   "(* (* ?a ?b) ?c)" => "(* ?a (* ?b ?c))"),
+        rewrite!("add-distrib"; "(* ?a (+ ?b ?c))" => "(+ (* ?a ?b) (* ?a ?c))"),
 
         // mod related
-        rewrite!("mod-sum";     "(% ?p (+ (% ?q ?a) ?b))"           => "(% ?p (+ ?a ?b))" if less_than("?p", "?q")),         // if q >= p
-        rewrite!("mod-sum-1";   "(% ?p (+ (% ?q ?a) (% ?q ?b)))"    => "(+ (% ?q ?a) (% ?q ?b))" if less_than("?q","?p")),  // if q  < p
-
+        // rewrite!("mod-sum";
+        //     "(% ?p (+ (% ?q ?a) ?b))" => "(% ?p (+ ?a ?b))" if less_than("?p", "?q")),        // if q > p
+        multi_rewrite!("mod-sum";
+            "?l = (< ?p ?q) = true, ?c = (% ?p (+ (% ?q ?a) ?b))" => "?c = (% ?p (+ ?a ?b))"),      // if q > p
+        rewrite!("mod-sum-1";
+            "(% ?p (+ (% ?q ?a) (% ?q ?b)))" => "(+ (% ?q ?a) (% ?q ?b))" if less_than("?q","?p")),  // if q < p
+        // conditionals
+        rewrite!("gt-lte"; "(> ?a ?b)" => "(<= ?b ?a)"),
+        rewrite!("lt-gte"; "(< ?a ?b)" => "(>= ?b ?a)"),
+        // rewrite!("gt-gte"; "(> ?a ?b)" => "(>= ?a (+ ?b 1))"),
+        // multi_rewrite!("trans"; "?p1 = (> ?a ?b) = true, ?p2 = (> ?b ?c) = true" => "?p3 = (> ?a (+ ?c 1)) = true"),
     ]
 }
 
@@ -86,12 +99,12 @@ fn less_than(a: &str, b: &str) -> impl Fn(&mut EGraph<ModIR, ModAnalysis>, Id, &
                     .and_then(|truth| Some(truth == comp_id))
             })
             .unwrap_or(false);
-        println!(
-            "Entering conditional {} < {} = {}",
-            egraph.id_to_expr(subst[a_var]),
-            egraph.id_to_expr(subst[b_var]),
-            res
-        );
+        // println!(
+        //     "Entering conditional {} < {} = {}",
+        //     egraph.id_to_expr(subst[a_var]),
+        //     egraph.id_to_expr(subst[b_var]),
+        //     res
+        // );
         res
     }
 }
@@ -105,10 +118,28 @@ fn check_equivalence(preconditions: &[&str], lhs: &str, rhs: &str) {
 
     let lhs_expr: RecExpr<ModIR> = lhs.parse().unwrap();
     let rhs_expr: RecExpr<ModIR> = rhs.parse().unwrap();
+
+    println!(
+        "Checking \n{}\n =?\n{}\n given the following conditions: {:?}",
+        lhs_expr.to_string(),
+        rhs_expr.to_string(),
+        preconditions
+    );
+
     let lhs_pattern = Pattern::from(&lhs_expr);
     let rhs_pattern = Pattern::from(&rhs_expr);
     let mut runner = Runner::default()
         .with_explanations_enabled()
+        .with_iter_limit(50)
+        .with_time_limit(Duration::from_secs(60))
+        .with_hook(|runner| {
+            runner
+                .egraph
+                .dot()
+                .to_pdf(format!("target/iter_{}.pdf", runner.iterations.len()))
+                .unwrap();
+            Ok(())
+        })
         // .without_explanation_length_optimization()
         .with_expr(&lhs_expr)
         .with_expr(&rhs_expr);
@@ -118,7 +149,6 @@ fn check_equivalence(preconditions: &[&str], lhs: &str, rhs: &str) {
     // add the preconditions to the truth values of the egraph
     let truth_id = runner.egraph.add(ModIR::Bool(true));
     for precond in &precond_exprs {
-        println!("{:?}", precond);
         let p_id = runner.egraph.add_expr(precond);
         runner.egraph.union(truth_id, p_id);
     }
@@ -126,6 +156,8 @@ fn check_equivalence(preconditions: &[&str], lhs: &str, rhs: &str) {
     // println!("Egraph post preconditions: {:#?}", runner.egraph);
 
     let mut runner = runner.run(rewrite_rules);
+
+    runner.egraph.dot().to_pdf("target/foo.pdf").unwrap();
 
     let equiv = !runner.egraph.equivs(&lhs_expr, &rhs_expr).is_empty();
 
@@ -163,11 +195,19 @@ fn check_equivalence(preconditions: &[&str], lhs: &str, rhs: &str) {
 }
 
 fn main() {
-    println!("Hello, world!");
-
     check_equivalence(
-        &["(< r q)"],
+        &["(> q r)", "(> r p)"],
         "(% r ( + (% p a) (% q (+ (% p b) (% p c)))))",
         "(% r ( + (% q (+ (% p a) (% p b))) (% p c)))",
     );
+
+    // check_equivalence(
+    //     &["(> q p)", "(> r (+ p q))", "(>= t (+ p p))"],
+    //     "(% r (*
+    //         (% p a)
+    //         (% q (+ (% p b) (% p c)))))",
+    //     "(% r (+
+    //         (% t (* (% p a) (% p b)))
+    //         (% t (* (% p a) (% p c)))))",
+    // );
 }
