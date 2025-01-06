d2830515eb82f643420cd3934affe6a8e8520cdf
luigirinaldi <luigirinaldi@users.noreply.github.com>
2025-01-06 10:53:40 +0100

    add signed interpretation and better condition matching

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::fmt::Debug;
+use std::hash::DefaultHasher;
 use std::str::FromStr;
+use std::sync::mpsc::RecvError;
 use std::time::Duration;
 
 use egg::*;
@@ -13,6 +16,7 @@ type Num = num::BigRational;
 define_language! {
     enum ModIR {
         "%" = Mod([Id; 2]), // mod operator to capture the bitwidth of a given sub-expression
+        "@" = Sign([Id; 2]),
         // Arithmetic operators
         "+" = Add([Id; 2]),
         "-" = Sub([Id; 2]),
@@ -57,6 +61,8 @@ fn rules() -> Vec<Rewrite<ModIR, ModAnalysis>> {
         // normal arithmetic
         rewrite!("add-comm";    "(+ ?a ?b)" => "(+ ?b ?a)"),
         rewrite!("add-assoc";   "(+ (+ ?a ?b) ?c)" => "(+ ?a (+ ?b ?c))"),
+        rewrite!("sub-comm";    "(- ?a ?b)" => "(- ?b ?a)"),
+        rewrite!("sub-assoc";   "(- (- ?a ?b) ?c)" => "(- ?a (- ?b ?c))"),
         rewrite!("mul-comm";    "(* ?a ?b)" => "(* ?b ?a)"),
         rewrite!("mul-assoc";   "(* (* ?a ?b) ?c)" => "(* ?a (* ?b ?c))"),
         // rewrite!("add-distrib";     "(* ?a (+ ?b ?c))" <=> "(+ (* ?a ?b) (* ?a ?c))"),
@@ -80,12 +86,180 @@ fn rules() -> Vec<Rewrite<ModIR, ModAnalysis>> {
         rewrite!("mod-mul-1";
             "(% ?q (* (% ?p ?a) ?b))" => "(% ?q (* ?a ?b))"
             if precondition(&["(>= ?p ?q)"])),
+
+        // sign related
+        rewrite!("signed";
+            "(@ ?s (% ?bw ?a))" => "(- (* 2 (% (- ?bw 1) ?a)) (% ?bw ?a))"
+            if precondition(&["(?s)"])
+        )
     ];
     // rules.extend(rewrite!("add-distrib";     "(* ?a (+ ?b ?c))" <=> "(+ (* ?a ?b) (* ?a ?c))"));
     // rules.extend(rewrite!("lt_gt"; "(> ?a ?b)" <=> "(< ?b ?a)"));
     rules.extend(rewrite!("gte-lt"; "(>= ?a ?b)" <=> "(< ?b ?a)"));
     rules.extend(rewrite!("mul-distrib"; "(+ (* ?a ?b) (* ?a ?c))" <=> "(* ?a (+ ?b ?c))"));
     rules
+}
+
+fn recursive_node_clone(
+    egraph: &EGraph<ModIR, ModAnalysis>,
+    root_id: &Id,
+    new_expr: &mut RecExpr<ModIR>,
+) -> Id {
+    // let recurse = |a: &Id, b: &Id| -> [Id; 2] {
+    //     let id_a = recursive_node_clone(egraph, a, new_expr);
+    //     let id_b = recursive_node_clone(egraph, b, new_expr);
+    //     [id_a, id_b]
+    // };
+    let root_node = egraph.id_to_node(*root_id);
+    match root_node {
+        ModIR::Var(s) => new_expr.add(ModIR::Var(*s)),
+        ModIR::Mod([a, b]) => {
+            let id_a = recursive_node_clone(egraph, a, new_expr);
+            let id_b = recursive_node_clone(egraph, b, new_expr);
+
+            new_expr.add(ModIR::Mod([id_a, id_b]))
+        }
+        ModIR::Sign([a, b]) => {
+            let id_a = recursive_node_clone(egraph, a, new_expr);
+            let id_b = recursive_node_clone(egraph, b, new_expr);
+
+            new_expr.add(ModIR::Sign([id_a, id_b]))
+        }
+        ModIR::Add([a, b]) => {
+            let id_a = recursive_node_clone(egraph, a, new_expr);
+            let id_b = recursive_node_clone(egraph, b, new_expr);
+
+            new_expr.add(ModIR::Add([id_a, id_b]))
+        }
+        ModIR::Sub([a, b]) => {
+            let id_a = recursive_node_clone(egraph, a, new_expr);
+            let id_b = recursive_node_clone(egraph, b, new_expr);
+
+            new_expr.add(ModIR::Sub([id_a, id_b]))
+        }
+        ModIR::Mul([a, b]) => {
+            let id_a = recursive_node_clone(egraph, a, new_expr);
+            let id_b = recursive_node_clone(egraph, b, new_expr);
+
+            new_expr.add(ModIR::Mul([id_a, id_b]))
+        }
+        ModIR::ShiftR([a, b]) => {
+            let id_a = recursive_node_clone(egraph, a, new_expr);
+            let id_b = recursive_node_clone(egraph, b, new_expr);
+
+            new_expr.add(ModIR::ShiftR([id_a, id_b]))
+        }
+        ModIR::ShiftL([a, b]) => {
+            let id_a = recursive_node_clone(egraph, a, new_expr);
+            let id_b = recursive_node_clone(egraph, b, new_expr);
+
+            new_expr.add(ModIR::ShiftL([id_a, id_b]))
+        }
+        ModIR::GT([a, b]) => {
+            let id_a = recursive_node_clone(egraph, a, new_expr);
+            let id_b = recursive_node_clone(egraph, b, new_expr);
+
+            new_expr.add(ModIR::GT([id_a, id_b]))
+        }
+        ModIR::GTE([a, b]) => {
+            let id_a = recursive_node_clone(egraph, a, new_expr);
+            let id_b = recursive_node_clone(egraph, b, new_expr);
+
+            new_expr.add(ModIR::GTE([id_a, id_b]))
+        }
+        ModIR::LT([a, b]) => {
+            let id_a = recursive_node_clone(egraph, a, new_expr);
+            let id_b = recursive_node_clone(egraph, b, new_expr);
+
+            new_expr.add(ModIR::LT([id_a, id_b]))
+        }
+        ModIR::LTE([a, b]) => {
+            let id_a = recursive_node_clone(egraph, a, new_expr);
+            let id_b = recursive_node_clone(egraph, b, new_expr);
+
+            new_expr.add(ModIR::LTE([id_a, id_b]))
+        }
+        ModIR::Bool(_bool) => new_expr.add(root_node.clone()),
+        ModIR::Num(_num) => new_expr.add(root_node.clone()),
+        _ => *root_id,
+    }
+}
+
+fn apply_subst(
+    egraph: &EGraph<ModIR, ModAnalysis>,
+    subst: &Subst,
+    base_expr: &RecExpr<ModIR>,
+    root_id: Id,
+    new_expr: &mut RecExpr<ModIR>,
+) -> Id {
+    // disgusting
+    let root_node = base_expr[root_id].clone();
+    match root_node {
+        ModIR::Var(s) => {
+            let var = Var::from_str(s.as_str()).unwrap();
+            // let new_node = egraph.id_to_node(*subst.get(var).unwrap());
+            // print!("{:#?} ,", new_node);
+            // new_expr.add(new_node.clone());
+            recursive_node_clone(egraph, subst.get(var).unwrap(), new_expr)
+        }
+        ModIR::Mod([a, b]) => {
+            let id_a = apply_subst(egraph, subst, base_expr, a, new_expr);
+            let id_b = apply_subst(egraph, subst, base_expr, b, new_expr);
+            new_expr.add(ModIR::Mod([id_a, id_b]))
+        }
+        ModIR::Sign([a, b]) => {
+            let id_a = apply_subst(egraph, subst, base_expr, a, new_expr);
+            let id_b = apply_subst(egraph, subst, base_expr, b, new_expr);
+            new_expr.add(ModIR::Sign([id_a, id_b]))
+        }
+        ModIR::Add([a, b]) => {
+            let id_a = apply_subst(egraph, subst, base_expr, a, new_expr);
+            let id_b = apply_subst(egraph, subst, base_expr, b, new_expr);
+            new_expr.add(ModIR::Add([id_a, id_b]))
+        }
+        ModIR::Sub([a, b]) => {
+            let id_a = apply_subst(egraph, subst, base_expr, a, new_expr);
+            let id_b = apply_subst(egraph, subst, base_expr, b, new_expr);
+            new_expr.add(ModIR::Sub([id_a, id_b]))
+        }
+        ModIR::Mul([a, b]) => {
+            let id_a = apply_subst(egraph, subst, base_expr, a, new_expr);
+            let id_b = apply_subst(egraph, subst, base_expr, b, new_expr);
+            new_expr.add(ModIR::Mul([id_a, id_b]))
+        }
+        ModIR::ShiftR([a, b]) => {
+            let id_a = apply_subst(egraph, subst, base_expr, a, new_expr);
+            let id_b = apply_subst(egraph, subst, base_expr, b, new_expr);
+            new_expr.add(ModIR::ShiftR([id_a, id_b]))
+        }
+        ModIR::ShiftL([a, b]) => {
+            let id_a = apply_subst(egraph, subst, base_expr, a, new_expr);
+            let id_b = apply_subst(egraph, subst, base_expr, b, new_expr);
+            new_expr.add(ModIR::ShiftL([id_a, id_b]))
+        }
+        ModIR::GT([a, b]) => {
+            let id_a = apply_subst(egraph, subst, base_expr, a, new_expr);
+            let id_b = apply_subst(egraph, subst, base_expr, b, new_expr);
+            new_expr.add(ModIR::GT([id_a, id_b]))
+        }
+        ModIR::GTE([a, b]) => {
+            let id_a = apply_subst(egraph, subst, base_expr, a, new_expr);
+            let id_b = apply_subst(egraph, subst, base_expr, b, new_expr);
+            new_expr.add(ModIR::GTE([id_a, id_b]))
+        }
+        ModIR::LT([a, b]) => {
+            let id_a = apply_subst(egraph, subst, base_expr, a, new_expr);
+            let id_b = apply_subst(egraph, subst, base_expr, b, new_expr);
+            new_expr.add(ModIR::LT([id_a, id_b]))
+        }
+        ModIR::LTE([a, b]) => {
+            let id_a = apply_subst(egraph, subst, base_expr, a, new_expr);
+            let id_b = apply_subst(egraph, subst, base_expr, b, new_expr);
+            new_expr.add(ModIR::LTE([id_a, id_b]))
+        }
+        ModIR::Bool(_bool) => new_expr.add(root_node.clone()),
+        ModIR::Num(num) => new_expr.add(ModIR::Num(num)),
+    }
 }
 
 // given a list of preconditions, returns a function that checks that they are all satisfied
@@ -95,19 +269,16 @@ fn precondition(conds: &[&str]) -> impl Fn(&mut EGraph<ModIR, ModAnalysis>, Id, 
     move |egraph, _root, subst| {
         let mut res = true;
         for expr in &cond_exprs {
-            let mut cond_subst = expr.clone();
+            let mut cond_subst: RecExpr<ModIR> = RecExpr::default();
 
-            for (id, node) in expr.items() {
-                match node {
-                    ModIR::Var(s) => {
-                        let var = Var::from_str(s.to_string().as_str()).unwrap();
-                        let new_node = egraph.id_to_node(*subst.get(var).unwrap());
-                        cond_subst[id] = new_node.clone();
-                    }
-                    _ => (),
-                }
-            }
+            apply_subst(egraph, subst, expr, expr.root(), &mut cond_subst);
 
+            println!(
+                "{:#?} => {:#?} {:#?}",
+                expr.to_string(),
+                cond_subst.to_string(),
+                cond_subst
+            );
             res &= egraph
                 .lookup_expr_ids(&cond_subst)
                 .and_then(|ids| {
@@ -205,7 +376,7 @@ fn check_equivalence(name_str: Option<&str>, preconditions: &[&str], lhs: &str, 
         if equiv { " " } else { " not " }
     );
 
-    let report = runner.report();
+    // let report = runner.report();
     // println!("{report}");
 
     let id = runner.egraph.find(*runner.roots.first().unwrap());
@@ -234,59 +405,80 @@ fn check_equivalence(name_str: Option<&str>, preconditions: &[&str], lhs: &str, 
 }
 
 fn main() {
+    // check_equivalence(
+    //     Some("assoc-1"),
+    //     &["(< r q)"],
+    //     "(% r ( + (% p a) (% q (+ (% p b) (% p c)))))",
+    //     "(% r ( + (% q (+ (% p a) (% p b))) (% p c)))",
+    // );
+
+    // check_equivalence(
+    //     Some("assoc-2"),
+    //     &["(< p q)", "(< s q)"],
+    //     "(% r ( + (% p a) (% q (+ (% p b) (% s c)))))",
+    //     "(% r ( + (% q (+ (% p a) (% p b))) (% s c)))",
+    // );
+
+    // check_equivalence(
+    //     Some("assoc-3"),
+    //     &["(< p q)", "(< s q)", "(< r u)"],
+    //     "(% r ( + (% p a) (% u (+ (% p b) (% s c)))))",
+    //     "(% r ( + (% q (+ (% p a) (% p b))) (% s c)))",
+    // );
+
+    // check_equivalence(
+    //     Some("multiply"),
+    //     &["(< p q)", "(> k (+ p p))"],
+    //     "(% r (*
+    //         (% p a)
+    //         (% q (+ (% p b) (% p c)))))",
+    //     "(% r (+
+    //         (% k (* (% p a) (% p b)))
+    //         (% k (* (% p a) (% p c)))))",
+    // );
+
+    // check_equivalence(
+    //     Some("multiply-2"),
+    //     &["(< r q)", "(< r k)"],
+    //     "(% r (*
+    //         (% p a)
+    //         (% q (+ (% p b) (% p c)))))",
+    //     "(% r (+
+    //         (% k (* (% p a) (% p b)))
+    //         (% k (* (% p a) (% p c)))))",
+    // );
+
+    // check_equivalence(
+    //     Some("multiply-3"),
+    //     &["(< r t)", "(< r u)", "(< r v)"],
+    //     "(% r (*
+    //         (% p a)
+    //         (% t (+ (% q b) (% s c)))))",
+    //     "(% r (+
+    //         (% u (* (% p a) (% q b)))
+    //         (% v (* (% p a) (% s c)))))",
+    // );
+
+    // check_equivalence(Some("mul"), &[], "(% r (* a 2))", "(% r (<< a 1))");
+
+    // check_equivalence(
+    //     Some("signed"),
+    //     &["sign"],
+    //     "(@ sign (% b a))",
+    //     "(- (* 2 (% (- b 1) a)) (% b a))",
+    // );
+
+    // check_equivalence(
+    //     Some("signed"),
+    //     &["sign"],
+    //     "(@ sign (% p (+ a b)))",
+    //     "(@ sign (% p (+ b a)))",
+    // );
+
     check_equivalence(
-        Some("assoc-1"),
-        &["(< r q)"],
+        Some("signed-assoc"),
+        &["(< r q)", "s"],
         "(% r ( + (% p a) (% q (+ (% p b) (% p c)))))",
         "(% r ( + (% q (+ (% p a) (% p b))) (% p c)))",
     );
-
-    check_equivalence(
-        Some("assoc-2"),
-        &["(< p q)", "(< s q)"],
-        "(% r ( + (% p a) (% q (+ (% p b) (% s c)))))",
-        "(% r ( + (% q (+ (% p a) (% p b))) (% s c)))",
-    );
-
-    check_equivalence(
-        Some("assoc-3"),
-        &["(< p q)", "(< s q)", "(< r u)"],
-        "(% r ( + (% p a) (% u (+ (% p b) (% s c)))))",
-        "(% r ( + (% q (+ (% p a) (% p b))) (% s c)))",
-    );
-
-    check_equivalence(
-        Some("multiply"),
-        &["(< p q)", "(> k (+ p p))"],
-        "(% r (*
-            (% p a)
-            (% q (+ (% p b) (% p c)))))",
-        "(% r (+
-            (% k (* (% p a) (% p b)))
-            (% k (* (% p a) (% p c)))))",
-    );
-
-    check_equivalence(
-        Some("multiply-2"),
-        &["(< r q)", "(< r k)"],
-        "(% r (*
-            (% p a)
-            (% q (+ (% p b) (% p c)))))",
-        "(% r (+
-            (% k (* (% p a) (% p b)))
-            (% k (* (% p a) (% p c)))))",
-    );
-
-    check_equivalence(
-        Some("multiply-3"),
-        &["(< r t)", "(< r u)", "(< r v)"],
-        "(% r (*
-            (% p a)
-            (% t (+ (% q b) (% s c)))))",
-        "(% r (+
-            (% u (* (% p a) (% q b)))
-            (% v (* (% p a) (% s c)))))",
-    );
-
-    check_equivalence(Some("mul"), &[], "(% r (* a 2))", "(% r (<< a 1))");
 }
