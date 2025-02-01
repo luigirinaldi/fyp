22c3bd3e10b844295971b17c2979a0921f9b91cc
luigirinaldi <luigirinaldi@users.noreply.github.com>
2025-02-01 18:24:32 +0000

    finish proofs for shifts

--- proofs/sufficient_conditions.thy
+++ proofs/sufficient_conditions.thy
@@ -109,9 +109,11 @@ by (smt (verit) bw_max_val mod_pos_pos_trivial pos_mod_sign power_increasing tha
 
 (* assuming b can always be cast to a nat *)
 definition "shl (a::int) (b::int) = a * 2^(nat(b))"
+definition "shr (a::int) (b::int) = a div 2^(nat(b))"
 value "nat(-3)"
 value "shl (-3) (-3)"
 value "shl (bw 2 3) (bw 3 (3))"
+value "shr (bw 8 48) (bw 3 (3))"
 
 (* using try *)
 theorem mult_by_two:
@@ -119,20 +121,88 @@ theorem mult_by_two:
 using bw_def shl_def by force
 
 (* Bitvector logic identities *)
-(*theorem merge_left_shift:
-"bw r (shl (bw u (shl (bw p a) (bw q b))) (bw s c)) = bw r (shl (bw p a) (bw t (bw q b + bw s c)))"
+
+(* manual proof *)
+theorem merge_left_shift:
+"bw r (shl (bw u (shl (bw p a) (bw q b))) (bw s c)) = bw r (shl (bw p a) (bw t (bw q b + bw s c)))" (is "?lhs = ?rhs")
 if "t > max q s" and "u \<ge> r"
-using bw_def shl_def add_full_prec add_remove_prec sorry
-*)
-
-
-(*theorem merge_left_shift:
-"bw r (bw u (bw p a * 2 ^ nat(bw q b)) * 2 ^ nat(bw s c)) = bw r (bw p a * 2^nat(bw t (bw q b + bw s c)))"
-if "t > max q s" and "u \<ge> r"*)
+proof - 
+have "?lhs = bw r ((bw p a * 2 ^ nat(bw q b)) * 2 ^ nat(bw s c))" using  add.right_neutral add_remove_prec that(2) mul_remove_prec shl_def by presburger
+moreover have "... = bw r(bw p a * 2 ^(nat(bw q b + bw s c)))" by (simp add: bw_def mult.commute mult.left_commute nat_add_distrib power_add)
+moreover have "... = ?rhs" using add_full_prec that(1) shl_def by simp
+ultimately show ?thesis by simp
+qed
 
 lemma div_gte:
-"bw p ((bw q a) div (bw r b)) = bw q a div (bw r b)"
+"bw p ((bw q a) div 2^nat(bw r b)) = bw q a div 2^nat(bw r b)"
 if "p \<ge> q"
-using bw_def bw_max_val
-by (smt (verit, ccfv_SIG) div_by_1 mod_div_trivial pos_imp_zdiv_nonneg_iff pos_mod_sign reduce_mod that zdiv_mono2 zero_less_power zmod_trivial_iff)
+proof - 
+have "(bw q a) div 2^nat(bw r b) \<le> 2^q - 1" using bw_max_val bw_def by (smt (verit) div_by_1 pos_imp_zdiv_nonneg_iff zdiv_mono2 zero_less_power)
+moreover have "... < 2^p" using that(1) by (smt (verit) power_increasing)
+ultimately show ?thesis by (simp add: bw_def pos_imp_zdiv_nonneg_iff)
+qed
+
+lemma mul_pow2:
+"bw s (bw p a * 2^nat(bw q b)) = bw p a * 2^nat(bw q b)"
+if "s \<ge> p + 2^q -1"
+proof - 
+have "(bw p a * 2^nat(bw q b)) \<le> (2^p - 1)*(2^(2^q - 1))" using bw_def bw_max_val by (metis add_le_imp_le_diff diff_ge_0_iff_ge less_iff_succ_less_eq mult_mono nat_less_numeral_power_cancel_iff one_le_numeral one_le_power power_increasing zero_le_numeral zero_le_power zle_diff1_eq)
+moreover have "... \<le> 2^(p + 2^q - 1) - 2^(2^q - 1)" by (metis (no_types, opaque_lifting) Nat.add_diff_assoc add_le_imp_le_diff diff_add_cancel left_diff_distrib' mult_1 nle_le one_le_numeral one_le_power power_add)
+moreover have "... \<le> 2^(p + 2^q - 1)" by simp
+moreover have "... \<le> 2^s" using that(1) by simp
+ultimately show ?thesis by (smt (verit) bw_def mod_pos_pos_trivial mult_nonneg_nonneg one_le_power pos_mod_sign)
+qed
+
+(* sledgehammer cannot solve it*)
+theorem merge_right_shift:
+"bw r (shr (bw u (shr (bw p a) (bw q b))) (bw s c)) = bw r (shr (bw p a) (bw t (bw q b + bw s c)))" (is "?lhs = ?rhs")
+if "t > max q s" and "u \<ge> p"
+proof - 
+have "?lhs = bw r ((bw u ((bw p a) div (2 ^ nat(bw q b)))) div 2 ^nat(bw s c))" using shr_def by auto
+moreover have "... =  bw r ( ((bw p a) div (2 ^ nat(bw q b))) div 2 ^nat(bw s c)) " using that(2) div_gte by auto
+moreover have "... = bw r ((bw p a) div (2 ^ nat(bw q b) * 2^nat(bw s c)))" by (simp add: div_exp_eq power_add)
+ultimately show ?thesis by (metis add_full_prec bw_def max_less_iff_conj nat_add_distrib not_exp_less_eq_0_int pos_mod_sign power_add shr_def that(1) verit_comp_simplify1(3))
+qed
+
+(* proven by sledgehammer*)
+theorem left_shift_add:
+"bw r ((shl (bw s (bw p a + bw q b)) (bw t c))) = bw r ((bw u (shl (bw p a) (bw t c))) + (bw u (shl (bw q b) (bw t c))))"
+if "s \<ge> r \<or> s > max p q" and "u \<ge> r"
+using shl_def by (smt (verit, ccfv_SIG) add.commute add_full_prec add_remove_prec int_distrib(1) max_less_iff_conj mul_remove_prec that(1) that(2))
+
+(* sledgehammer *)
+theorem left_shift_mult:
+"bw r ((shl (bw t (bw p a * bw q b)) (bw t c))) = bw r (bw v (shl (bw p a) (bw t c)) * (bw q b))" 
+if "v \<ge> r" and "t \<ge> r"
+using shl_def by (metis ab_semigroup_mult_class.mult_ac(1) mul_remove_prec mult.commute that(1) that(2))
+
+theorem add_right_shift:
+"bw r (
+    (bw p a)
+    + 
+  bw q (
+    (shr (bw t b) (bw u c))
+  )
+) 
+= 
+bw r (
+  shr 
+    (bw v 
+      (
+        (bw s (shl (bw p a) (bw u c)))
+        + 
+        (bw t b)
+      )
+    ) 
+    (bw u c)
+  )" (is "?lhs = ?rhs")
+  if "q \<ge> t" and "v > max s t" and "s \<ge> p + 2^u - 1" and "u > 0"
+proof - 
+have "?lhs = bw r ((bw p a) + (bw q ((bw t b) div 2^nat(bw u c))))" using shr_def by simp
+moreover have "... =  bw r (((bw p a) * 2^nat(bw u c)) div 2^nat(bw u c)  + (bw q ((bw t b) div 2^nat(bw u c))))" by simp
+(* found by simp after introducing the above step of multiplying and dividing by the same number *) 
+moreover have "... = ?rhs" by (smt (verit, del_insts) add_full_prec calculation(2) div_gte div_mult_self1 max.strict_boundedE mul_pow2 power_eq_0_iff shl_def shr_def that(1) that(2) that(3))
+ultimately show ?thesis by argo
+qed
+
 
