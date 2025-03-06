b306548b15f0ad07ed7e0c828e8825bddd7d42fb
luigirinaldi <luigirinaldi@users.noreply.github.com>
2025-03-06 15:47:04 +0000

    reorganize proofs wip

--- proofs/manual_rewrite.thy
+++ proofs/manual_rewrite.thy
@@ -1,15 +1,8 @@
-theory sufficient_conditions imports Main begin
-
-definition "bw (p::nat) (a::int) = a mod 2^p"
-
-value "bw 3 (-23)"
-
-lemma bw_max_val:
-"bw p a \<le> 2^p - 1"
-using bw_def by simp
+theory manual_rewrite
+    imports simp_rewrite_lemmas 
+begin
 
 (* Bitvector Arithmetic Identities *)
-
 theorem add_comm:
 "bw r (bw p a + bw q b) = bw r (bw q b + bw p a)"
 by (simp add: add.commute)
@@ -18,45 +11,11 @@ theorem mul_comm:
 "bw r (bw p a * bw q b) = bw r (bw q b * bw p a)"
 by (simp add: mult.commute)
 
-lemma mul_full_prec:
-"bw p (bw q a * bw r b) = (bw q a * bw r b)"
-if "q + r \<le> p"
-proof -
-have "bw q a * bw r b \<le> (2^q - 1) * (2^r - 1)" using bw_def bw_max_val by (simp add: mult_mono)
-(* found with try *)
-also have "... < 2^(q + r)" by (smt (verit, best) distrib_left left_diff_distrib' mult_mono one_le_power one_power2 power_add)
-(* found with try *)
-then show ?thesis by (smt (verit, ccfv_SIG) bw_def calculation mod_pos_pos_trivial mult_nonneg_nonneg pos_mod_sign power_increasing that zero_less_power)
-qed
-
-(* found using try*)
-lemma mul_remove_prec:
-"bw p (bw q a *  b) = bw p ( a * b)"
-if "q \<ge> p"
-by (metis bw_def le_imp_power_dvd mod_mod_cancel mod_mult_cong that)
-
 (* found using try after defining the helper lemmas*)
 theorem mul_assoc:
 "bw t (bw u (bw p a * bw r b) * bw s c) = bw t (bw p a * bw q (bw r b * bw s c))"
 if "q \<ge> t \<or> (r+s) \<le> q" and "u \<ge> t \<or> (p+r) \<le> u"
 by (smt (verit, del_insts) ab_semigroup_mult_class.mult_ac(1) mul_full_prec mul_remove_prec mult.commute that(1) that(2))
-
-lemma add_full_prec:
-"bw p (bw q a + bw r b) = (bw q a + bw r b)"
-if "q < p \<and> r < p"
-proof -
-have "bw q a + bw r b \<le> (2^q - 1) + (2^r - 1)" using bw_def bw_max_val by (meson add_mono)
-(* found with try *)
-moreover have "... \<le> (2^(p-1) - 1) + (2^(p-1) - 1)" using that by (metis ab_group_add_class.ab_diff_conv_add_uminus add_le_imp_le_diff add_mono add_right_mono less_iff_succ_less_eq one_le_numeral power_increasing)
-moreover have "... < 2^p" by (simp add: power_eq_if)
-ultimately show ?thesis by (simp add: bw_def)
-qed
-
-(* found using try*)
-lemma add_remove_prec:
-"bw p (bw q a + b) = bw p (a + b)"
-if "q \<ge> p"
-by (metis bw_def le_imp_power_dvd mod_add_cong mod_mod_cancel that)
 
 (* found using try *)
 theorem add_assoc:
@@ -101,20 +60,6 @@ theorem mult_by_one:
 if "bw p 1 = bw p b" and "p > 0" and "q > 0"
 using bw_def by (smt (verit, best) le_imp_power_dvd mod_mod_cancel mul_remove_prec mult.commute mult_cancel_right1 one_mod_2_pow_eq power_increasing_iff that(1) that(2) that(3))
 
-lemma reduce_mod:
-"bw p (bw q a) = bw q a"
-if "p \<ge> q"
-using bw_def 
-by (smt (verit) bw_max_val mod_pos_pos_trivial pos_mod_sign power_increasing that zero_less_power)
-
-(* assuming b can always be cast to a nat *)
-definition "shl (a::int) (b::int) = a * 2^(nat(b))"
-definition "shr (a::int) (b::int) = a div 2^(nat(b))"
-value "nat(-3)"
-value "shl (-3) (-3)"
-value "shl (bw 2 3) (bw 3 (3))"
-value "shr (bw 8 48) (bw 3 (3))"
-
 (* using try *)
 theorem mult_by_two:
 "bw r (bw p a * bw 2 2) = bw r(shl (bw p a) 1)"
@@ -133,30 +78,11 @@ moreover have "... = ?rhs" using add_full_prec that(1) shl_def by simp
 ultimately show ?thesis by simp
 qed
 
-lemma div_gte:
-"bw p ((bw q a) div 2^nat(bw r b)) = bw q a div 2^nat(bw r b)"
-if "p \<ge> q"
-proof - 
-have "(bw q a) div 2^nat(bw r b) \<le> 2^q - 1" using bw_max_val bw_def by (smt (verit) div_by_1 pos_imp_zdiv_nonneg_iff zdiv_mono2 zero_less_power)
-moreover have "... < 2^p" using that(1) by (smt (verit) power_increasing)
-ultimately show ?thesis by (simp add: bw_def pos_imp_zdiv_nonneg_iff)
-qed
-
-lemma mul_pow2:
-"bw s (bw p a * 2^nat(bw q b)) = bw p a * 2^nat(bw q b)"
-if "s \<ge> p + 2^q -1"
-proof - 
-have "(bw p a * 2^nat(bw q b)) \<le> (2^p - 1)*(2^(2^q - 1))" using bw_def bw_max_val by (metis add_le_imp_le_diff diff_ge_0_iff_ge less_iff_succ_less_eq mult_mono nat_less_numeral_power_cancel_iff one_le_numeral one_le_power power_increasing zero_le_numeral zero_le_power zle_diff1_eq)
-moreover have "... \<le> 2^(p + 2^q - 1) - 2^(2^q - 1)" by (metis (no_types, opaque_lifting) Nat.add_diff_assoc add_le_imp_le_diff diff_add_cancel left_diff_distrib' mult_1 nle_le one_le_numeral one_le_power power_add)
-moreover have "... \<le> 2^(p + 2^q - 1)" by simp
-moreover have "... \<le> 2^s" using that(1) by simp
-ultimately show ?thesis by (smt (verit) bw_def mod_pos_pos_trivial mult_nonneg_nonneg one_le_power pos_mod_sign)
-qed
-
 (* sledgehammer cannot solve it*)
 theorem merge_right_shift:
 "bw r (shr (bw u (shr (bw p a) (bw q b))) (bw s c)) = bw r (shr (bw p a) (bw t (bw q b + bw s c)))" (is "?lhs = ?rhs")
 if "t > max q s" and "u \<ge> p"
+
 proof - 
 have "?lhs = bw r ((bw u ((bw p a) div (2 ^ nat(bw q b)))) div 2 ^nat(bw s c))" using shr_def by auto
 moreover have "... =  bw r ( ((bw p a) div (2 ^ nat(bw q b))) div 2 ^nat(bw s c)) " using that(2) div_gte by auto
@@ -204,5 +130,6 @@ moreover have "... =  bw r (((bw p a) * 2^nat(bw u c)) div 2^nat(bw u c)  + (bw 
 moreover have "... = ?rhs" by (smt (verit, del_insts) add_full_prec calculation(2) div_gte div_mult_self1 max.strict_boundedE mul_pow2 power_eq_0_iff shl_def shr_def that(1) that(2) that(3))
 ultimately show ?thesis by argo
 qed
+end
 
 
---
+++ proofs/simp_rewrite_lemmas.thy
@@ -0,0 +1,75 @@
+theory simp_rewrite_lemmas
+    imports Main 
+begin
+
+definition "bw (p::nat) (a::int) = a mod 2^p"
+(* assuming b can always be cast to a nat *)
+definition "shl (a::int) (b::int) = a * 2^(nat(b))"
+definition "shr (a::int) (b::int) = a div 2^(nat(b))"
+
+lemma bw_max_val:
+"bw p a \<le> 2^p - 1"
+using bw_def by simp
+
+lemma mul_full_prec:
+"bw p (bw q a * bw r b) = (bw q a * bw r b)"
+if "q + r \<le> p"
+proof -
+have "bw q a * bw r b \<le> (2^q - 1) * (2^r - 1)" using bw_def bw_max_val by (simp add: mult_mono)
+(* found with try *)
+also have "... < 2^(q + r)" by (smt (verit, best) distrib_left left_diff_distrib' mult_mono one_le_power one_power2 power_add)
+(* found with try *)
+then show ?thesis by (smt (verit, ccfv_SIG) bw_def calculation mod_pos_pos_trivial mult_nonneg_nonneg pos_mod_sign power_increasing that zero_less_power)
+qed
+
+lemma mul_remove_prec:
+"bw p (bw q a *  b) = bw p ( a * b)"
+if "q \<ge> p"
+by (metis bw_def le_imp_power_dvd mod_mod_cancel mod_mult_cong that)
+
+lemma add_full_prec:
+"bw p (bw q a + bw r b) = (bw q a + bw r b)"
+if "q < p \<and> r < p"
+proof -
+have "bw q a + bw r b \<le> (2^q - 1) + (2^r - 1)" using bw_def bw_max_val by (meson add_mono)
+(* found with try *)
+moreover have "... \<le> (2^(p-1) - 1) + (2^(p-1) - 1)" using that by (metis ab_group_add_class.ab_diff_conv_add_uminus add_le_imp_le_diff add_mono add_right_mono less_iff_succ_less_eq one_le_numeral power_increasing)
+moreover have "... < 2^p" by (simp add: power_eq_if)
+ultimately show ?thesis by (simp add: bw_def)
+qed
+
+lemma add_remove_prec:
+"bw p (bw q a + b) = bw p (a + b)"
+if "q \<ge> p"
+by (metis bw_def le_imp_power_dvd mod_add_cong mod_mod_cancel that)
+
+lemma reduce_mod:
+"bw p (bw q a) = bw q a"
+if "p \<ge> q"
+using bw_def 
+by (smt (verit) bw_max_val mod_pos_pos_trivial pos_mod_sign power_increasing that zero_less_power)
+
+
+lemma div_gte:
+"bw p ((bw q a) div 2^nat(bw r b)) = bw q a div 2^nat(bw r b)"
+if "p \<ge> q"
+proof - 
+have "(bw q a) div 2^nat(bw r b) \<le> 2^q - 1" using bw_max_val bw_def by (smt (verit) div_by_1 pos_imp_zdiv_nonneg_iff zdiv_mono2 zero_less_power)
+moreover have "... < 2^p" using that(1) by (smt (verit) power_increasing)
+ultimately show ?thesis by (simp add: bw_def pos_imp_zdiv_nonneg_iff)
+qed
+
+lemma mul_pow2:
+"bw s (bw p a * 2^nat(bw q b)) = bw p a * 2^nat(bw q b)"
+if "s \<ge> p + 2^q -1"
+proof - 
+have "(bw p a * 2^nat(bw q b)) \<le> (2^p - 1)*(2^(2^q - 1))" using bw_def bw_max_val by (metis add_le_imp_le_diff diff_ge_0_iff_ge less_iff_succ_less_eq mult_mono nat_less_numeral_power_cancel_iff one_le_numeral one_le_power power_increasing zero_le_numeral zero_le_power zle_diff1_eq)
+moreover have "... \<le> 2^(p + 2^q - 1) - 2^(2^q - 1)" by (metis (no_types, opaque_lifting) Nat.add_diff_assoc add_le_imp_le_diff diff_add_cancel left_diff_distrib' mult_1 nle_le one_le_numeral one_le_power power_add)
+moreover have "... \<le> 2^(p + 2^q - 1)" by simp
+moreover have "... \<le> 2^s" using that(1) by simp
+ultimately show ?thesis by (smt (verit) bw_def mod_pos_pos_trivial mult_nonneg_nonneg one_le_power pos_mod_sign)
+qed
+
+end
+
+
