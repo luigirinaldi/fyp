c5f7b5d6a2e02ee28d86d2d0c3de7a7a0a5ab7c8
luigirinaldi <luigirinaldi@users.noreply.github.com>
2025-06-03 18:55:16 +0000

    run for 10 minutes

--- gen_graphs/gen_data.sh
+++ gen_graphs/gen_data.sh
@@ -64,7 +64,7 @@ run_mirabelle() {
     ulimit -v 16106127360
     stdbuf -oL /usr/bin/time --output="$sys_data" -a --format="{\"name\":\"$name_only\", \"cpu\":\"%P\", \"mem\":%M}," \
       "$ISABELLE_PATH" mirabelle -d "$DIR" -O "$DIR/mirabelle_out" \
-      -A "try0" -A "sledgehammer[timeout=$SLEDGEHAMMER_TIMEOUT]" \
+      -A "try0" -A "sledgehammer[timeout=$SLEDGEHAMMER_TIMEOUT, max_proofs=1]" \
       -T "$name_only" LemmaSledge | sed 's/^/[mirabelle] /'
   done
 
