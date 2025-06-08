0308f88a3812563e2b45dfe87924eb51aad71c64
luigirinaldi <luigirinaldi@users.noreply.github.com>
2025-06-08 23:22:56 +0100

    prove some more axioms

--- proofs/rewrite_lemmas.thy
+++ proofs/rewrite_lemmas.thy
@@ -172,4 +172,9 @@ lemma xor_and_or_help: "and (or a b) (or (not a) (not b)) = xor a b" for a b :: 
 lemma xor_and_or: "and (or (bw p a) (bw p b)) (or (bw p (not (bw p a))) (bw p (not (bw p b)))) = xor (bw p a) (bw p b)" for a b :: int 
 using xor_and_or_help by (metis bw_def take_bit_and take_bit_eq_mod take_bit_not_take_bit take_bit_or take_bit_xor)
 
+lemma and_self: "and a a = a" by simp
+lemma or_self: "or a a = a" by simp
+lemma and_not_self: "(and (bw p a) (bw p (not (bw p a)))) = 0" by (metis bit.xor_self or_self xor_and_or)
+lemma or_not_self: "(or (bw p a) (bw p (not (bw p a)))) = bw p (-1)" by (metis bit.disj_cancel_right bw_def take_bit_eq_mod take_bit_not_take_bit take_bit_or)
+
 end
