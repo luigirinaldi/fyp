7e8b92fe3ded906724e76af7190c0069b35e1e05
luigirinaldi <luigirinaldi@users.noreply.github.com>
2025-06-10 17:21:43 +0100

    update stats collection to get memory

---
+++ gen_graphs/scripts/get_data/get_egraph_data.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# --- Input Arguments ---
+# Usage: ./script.sh (BIN_PATH)
+
+SRC_DIR="./test_data"
+DEST_DIR="./gen_graphs/data"
+
+BIN_PATH="./target/release/hello-world"
+
+cargo build --release
+
+# --- Get Timings ---
+
+find "$SRC_DIR" -type f -name "*.json" | while read -r file; do
+  base_name=$(basename "$file" .json)
+
+  OUT_DIR="$DEST_DIR/${base_name}"
+
+  echo "Gathering data for $base_name"
+
+  "$BIN_PATH" "$file" --runner-stats "$OUT_DIR/egraph_stats.json" > /dev/null
+done
+
+cargo build --release --features get-heap-info
+
+# --- Get Meminfo ---
+
+find "$SRC_DIR" -type f -name "*.json" | while read -r file; do
+  base_name=$(basename "$file" .json)
+
+  OUT_DIR="$DEST_DIR/${base_name}"
+
+  echo "Gathering mem data for $base_name"
+
+  "$BIN_PATH" "$file" --runner-stats "$OUT_DIR/egraph_stats_mem.json" > /dev/null
+done
