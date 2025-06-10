ca5a4337bbab3028ef3838405a5cfd841c8d774b
luigirinaldi <luigirinaldi@users.noreply.github.com>
2025-06-10 20:36:47 +0100

    generating nice table and graphs

--- gen_graphs/scripts/graphing/utils.py
+++ gen_graphs/scripts/graphing/utils.py
@@ -5,4 +5,5 @@ def load_egraph_data(filename) -> dict:
     with open(filename, 'r') as f:
         data = json.load(f)
 
-    return {theorem: float(entry['summary']['total_time']) for theorem, entry in data.items() if entry['summary']['stop_reason'] == {"Other":"Found equivalence"}}
+
+    return data
