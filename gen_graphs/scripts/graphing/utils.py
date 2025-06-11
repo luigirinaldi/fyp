7a0eb124eb87ebcb8dda19ddfbc3bcd8b4f02fe8
luigirinaldi <luigirinaldi@users.noreply.github.com>
2025-06-12 00:42:18 +0100

    update scripts

--- gen_graphs/scripts/graphing/utils.py
+++ gen_graphs/scripts/graphing/utils.py
@@ -1,4 +1,21 @@
 import json
+import os
+
+def get_egraph_from_dir(data_dir = "../../data/"):
+    data_dir = "../../data/"
+
+    egraph_data = {}
+
+    for direc in os.listdir(data_dir):
+        try:
+            egraph_data[direc] = (
+            load_egraph_data(f"{data_dir}{direc}/egraph_stats.json"),
+            load_egraph_data(f"{data_dir}{direc}/egraph_stats_mem.json"),
+        )
+        except:
+            continue
+
+    return egraph_data
 
 # Load times from E-Graph file
 def load_egraph_data(filename) -> dict:
