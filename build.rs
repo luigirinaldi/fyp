358397c453943331c40cd25dce9377e62f162a9a
luigirinaldi <luigirinaldi@users.noreply.github.com>
2026-01-27 11:12:32 +0000

    working tests

--- build.rs
+++ build.rs
@@ -155,7 +155,7 @@ fn {fn_name}_validate() {{
 }}
 
 #[test]
-{should_panic_attr}#[timeout(7000)]
+{should_panic_attr}#[timeout(10000)]
 #[allow(non_snake_case)]
 fn {fn_name}_find_equiv() {{
     let mut eq = Equivalence::new(
